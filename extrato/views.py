68299417ed24efd12ea987d47b07f41951504d83
Winkler30 <charleswinkler76@gmail.com>
2024-06-12 19:35:41 -0300

    url novo_valor adicionada

--- extrato/views.py
+++ extrato/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from perfil.models import Categoria, Conta
 
 # Create your views here.
+def novo_valor(request):
+    if request.method == 'GET':
+        categorias = Categoria.objects.all()
+        contas = Conta.objects.all()
+        return render(request, 'novo_valor.html', {'categorias':categorias, 'contas':contas})
