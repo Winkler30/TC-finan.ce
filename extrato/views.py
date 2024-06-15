1cde774bcb4833f58c9da382560c987f6f54b96f
Winkler30 <charleswinkler76@gmail.com>
2024-06-15 15:46:11 -0300

    finalizando url novo_valor

--- extrato/views.py
+++ extrato/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from perfil.models import Categoria, Conta
+from .models import Valores
+from django.contrib import messages
+from django.contrib.messages import constants
 
 # Create your views here.
 def novo_valor(request):
@@ -7,3 +11,35 @@ def novo_valor(request):
         categorias = Categoria.objects.all()
         contas = Conta.objects.all()
         return render(request, 'novo_valor.html', {'categorias':categorias, 'contas':contas})
+    elif request.method == 'POST':
+        valor = request.POST.get('valor')
+        categoria = request.POST.get('categoria')
+        descricao = request.POST.get('descricao')
+        data = request.POST.get('data')
+        conta = request.POST.get('conta')
+        tipo = request.POST.get('tipo')
+
+        valores = Valores(
+            valor=valor,
+            categoria_id=categoria,
+            descricao=descricao,
+            data=data,
+            conta_id=conta,
+            tipo=tipo
+        )
+
+        valores.save()
+        
+        conta = Conta.objects.get(id=conta)
+
+        if tipo == 'E':
+            conta.valor += int(valor)
+        else:
+            conta.valor -= int(valor)
+
+        conta.save()
+
+        
+
+        messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso')
+        return redirect('/extrato/novo_valor')
