12834a64776ee9092b5e9301de5e912ec3035eb1
Winkler30 <charleswinkler76@gmail.com>
2024-06-15 18:01:10 -0300

    finalizacao do app extrato

--- extrato/views.py
+++ extrato/views.py
@@ -1,10 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from django.shortcuts import render, redirect
 from perfil.models import Categoria, Conta
 from .models import Valores
 from django.contrib import messages
 from django.contrib.messages import constants
-
+from datetime import datetime
+from django.template.loader import render_to_string
+import os
+from django.conf import settings
+from weasyprint import HTML
+from io import BytesIO
 # Create your views here.
 def novo_valor(request):
     if request.method == 'GET':
@@ -47,5 +52,35 @@ def novo_valor(request):
 
 
 def view_extrato(request):
-    valores = Valores.objects.all()
-    return render(request, 'view_extrato.html', {'valores':valores})
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+    conta_get = request.GET.get('conta')
+    categoria_get = request.GET.get('categoria')
+
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+
+
+    if conta_get:
+        valores = valores.filter(conta_id=conta_get)
+
+    if categoria_get:
+        valores = valores.filter(categoria__id=categoria_get)
+
+    return render(request, 'view_extrato.html', {'valores':valores, 'contas':contas, 'categorias':categorias})
+
+
+def exportar_pdf(request):
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+    
+    path_template = os.path.join(settings.BASE_DIR, 'templates/partials/extrato.html')
+    path_output = BytesIO()
+
+    template_render = render_to_string(path_template, {'valores': valores, 'contas': contas, 'categorias': categorias})
+    HTML(string=template_render).write_pdf(path_output)
+
+    path_output.seek(0)
+    
+
+    return FileResponse(path_output, filename="extrato.pdf")
