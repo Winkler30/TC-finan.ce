2f480aa7639e9548fd18ba4d490140e74e76b42e
Winkler30 <charleswinkler76@gmail.com>
2024-06-16 17:49:04 -0300

    adicao do botao voltar e inicio do app contas

---
+++ contas/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from perfil.models import Categoria
+
+# Create your views here.
+def definir_contas(request):
+    if request.method == "GET":
+        categorias = Categoria.objects.all()
+        return render(request, 'definir_contas.html', {'categorias': categorias})
